20e6f552c591280d4ae7e312e619a467a4a33182
Sajesh1234 <69909148+Sajesh1234@users.noreply.github.com>
2020-08-19 10:21:52 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/**************************************************
+* Hello.java
+***************************************************
+* Sajesh Sahoo
+* 8/19/2020
+* CMSC 255 - 003
+***************************************************/
+
+public class Hello{
+
+	public static void main (String[] args){
+	
+		System.out.println("<Sajesh Sahoo>");
+		System.out.println("<Lab 01>");
+		System.out.println("<I Created My First Java Program>");
+		System.out.println("<8/19/2020>");
+		System.out.println("<CMSC 255 - 003 Lab 01>");
+		System.out.println();
+		}
+	
+	}
+	
